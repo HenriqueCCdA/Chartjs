349e47c1a3faa6655c1679552e8206d15f8402bb
Henrique de Andrade <37959973+HenriqueCCdA@users.noreply.github.com>
2021-11-14 18:46:03 -0300

    Iniciado.

---
+++ devAnalytics/README.md
@@ -0,0 +1,7 @@
+O codigo desenvolvido aqui foi baseado no video [Javascript - Utilizando a lib chart.js](https://www.youtube.com/watch?v=OnmaeSnIEek)
+
+Instalado o Chart.js com o **npm**:
+
+```console
+npm install chart.js --save
+```
---
+++ devAnalytics/js/index.js
@@ -0,0 +1,79 @@
+let ctx = document.getElementById('chart').getContext('2d');
+
+let chart = new Chart(ctx, {
+    type: 'line',
+    data:{
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(41, 128, 185)',
+            borderColor: 'rgb(41, 128, 185)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }],
+    },
+
+    options: {
+        animation: {
+            duration: 2000,
+            easing: 'easeOutBounce'
+        },
+        layout: {
+            padding: {
+                left: 20,
+                right:20,
+                top: 20,
+                bottom:20
+            }
+        },
+        plugins: {
+            title: {
+                display: true,
+                text: 'Sales by Months',
+                fontSize: 20,
+            },
+            legend:{
+                display: true,
+                position: 'bottom'
+            },
+            tooltip:{
+                enabled: true,
+                intersect: true,
+                backgroundColor: 'rgba(41, 128, 185, 0.8)'
+            },
+        },
+        scales: {
+            x : {
+                grid:{ 
+                    display: false
+                }
+            },
+            y : {
+                grid:{ 
+                    display: false
+                }
+            }
+        }
+    }
+});
+
+/* const addData = () => {
+    let sizeData = chart.data.datasets[0].data.length
+    console.log(chart.data.datasets[0].data)
+    chart.data.datasets[0].data[sizeData] = Math.random() * 100;
+    chart.data.labels[sizeData] = `New Data ${sizeData + 1}`
+    chart.update()
+} */
+
+function addData(){
+    let sizeData = chart.data.datasets[0].data.length
+    console.log(chart.data.datasets[0].data)
+    chart.data.datasets[0].data[sizeData] = Math.random() * 100;
+    chart.data.labels[sizeData] = `New Data ${sizeData + 1}`
+    chart.update()
+}
+
+const removeData = () => {
+    chart.data.datasets[0].data.pop()
+    chart.data.labels.pop()
+    chart.update()
+}
